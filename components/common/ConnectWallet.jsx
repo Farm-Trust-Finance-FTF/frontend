af33f5ae687d339fd56e8662427a1285c4edab54
MarsIfeanyi <marschalice@gmail.com>
2023-12-03 14:02:41 +0100

    Integrate WalletConnection

--- components/common/ConnectWallet.jsx
+++ components/common/ConnectWallet.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function ConnectWallet() {
   return (
     <div>
-      <button className='bg-[#306621] text-white px-8 py-3 rounded-full'>Connect Wallet</button>
+      {/* <button className="bg-[#306621] text-white px-8 py-3 rounded-full">
+        Connect Wallet
+      </button> */}
+      <ConnectButton />
     </div>
-  )
+  );
 }
 
-export default ConnectWallet
+export default ConnectWallet;
