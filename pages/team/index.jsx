1540b4fe990c7e56f068a292521fee4cc2bad9a4
MarsIfeanyi <marschalice@gmail.com>
2023-12-03 14:07:30 +0100

    Create Team page

---
+++ pages/team/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className=" flex text-xl font-bold mt-10 items-center justify-center">
+      Meet the Team
+    </div>
+  );
+};
+
+export default index;
