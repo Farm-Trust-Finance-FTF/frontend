fd40d3c3130a9694368c2159fb89cf75e3accdf6
MarsIfeanyi <marschalice@gmail.com>
2023-12-03 14:03:34 +0100

    Add Lending, Insuance and Team and Link to pages

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Logo from './Logo'
-import ConnectWallet from './ConnectWallet'
+import React from "react";
+import Logo from "./Logo";
+import ConnectWallet from "./ConnectWallet";
+import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className='hidden padding text-[#141414] mt-5 lg:flex justify-between items-center'>
+    <nav className="hidden padding text-[#141414] mt-5 lg:flex justify-between items-center ">
       <Logo />
-      <div className='flex space-x-5 cursor-pointer font-bold'>
+      <div className="flex space-x-10 cursor-pointer font-bold ">
         <h1>About</h1>
         <h1>Contact</h1>
+        <Link href="lending-and-borrowing">Lending</Link>
+        <Link href="parametric-insurance">Insurance</Link>
+        <Link href="team">Team</Link>
       </div>
       <ConnectWallet />
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
