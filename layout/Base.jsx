338eeaa0ca3d2bd52af434e585d56664652fcf4c
MarsIfeanyi <marschalice@gmail.com>
2023-12-03 23:46:19 +0100

    Fix Bugs for Failed Production

--- layout/Base.jsx
+++ layout/Base.jsx
@@ -1,16 +1,16 @@
-import Footer from "@/components/common/Footer"
-import MobileHeader from "@/components/common/MobileHeader"
-import Navbar from "@/components/common/Navbar"
+import Footer from "components/common/Footer";
+import MobileHeader from "components/common/MobileHeader";
+import Navbar from "components/common/Navbar";
 
 function BaseLayout({ children }) {
   return (
     <>
       <Navbar />
       <MobileHeader />
-      <main className="padding">{ children }</main>
+      <main className="padding">{children}</main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default BaseLayout
+export default BaseLayout;
