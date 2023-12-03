0a19bc93aa67ab124359534cedb93570b50f398e
MarsIfeanyi <marschalice@gmail.com>
2023-12-03 14:06:35 +0100

    Create lendng and borowing Page

---
+++ pages/lending-and-borrowing/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className=" flex text-xl font-bold mt-10 items-center justify-center">
+      Lending and Borrowing Page
+    </div>
+  );
+};
+
+export default index;
