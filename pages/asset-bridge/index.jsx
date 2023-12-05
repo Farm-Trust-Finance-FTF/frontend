f5390970a119941e0c2d72e386ff6ad5a50d2af6
MarsIfeanyi <marschalice@gmail.com>
2023-12-05 22:45:21 +0100

    create asset bridge page

---
+++ pages/asset-bridge/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div>
+      <h1 className=" flex text-xl font-bold mt-10 items-center justify-center">
+        CrossChain Asset Bridge Page
+      </h1>
+    </div>
+  );
+};
+
+export default index;
