b9698d0503fbdda36da745e302e59411a8bfb8c2
MarsIfeanyi <marschalice@gmail.com>
2023-12-05 23:31:44 +0100

    Add Contract Address and ABI

---
+++ constants/index.jsx
@@ -0,0 +1,11 @@
+import { LendingBorrowing_ADDRESS } from "./lendingBorrowingAddress";
+import LendingBorrowingABI from "./lendingBorrowingABI.json";
+import { Insurance_ADDRESS } from "./insuranceAddress";
+import InsuranceABI from "./insuranceABI.json";
+
+export {
+  LendingBorrowing_ADDRESS,
+  LendingBorrowingABI,
+  Insurance_ADDRESS,
+  InsuranceABI,
+};
---
+++ constants/lendingBorrowingAddress.jsx
@@ -0,0 +1,1 @@
+export const LendingBorrowing_ADDRESS = "0x";
