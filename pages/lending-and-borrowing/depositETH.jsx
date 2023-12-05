381b81ec7e2e1abfa7dba3adfb807db14df8cfa9
MarsIfeanyi <marschalice@gmail.com>
2023-12-05 22:54:42 +0100

    Create depositETH page

---
+++ pages/lending-and-borrowing/depositETH.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DepositETH from "../../components/lending/DepositETH";
+
+const depositETH = () => {
+  return (
+    <div>
+      <DepositETH />
+    </div>
+  );
+};
+
+export default depositETH;
