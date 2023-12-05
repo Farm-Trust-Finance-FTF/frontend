c29da7f075a71d24eb804d65215bcaa5765b6a1e
MarsIfeanyi <marschalice@gmail.com>
2023-12-05 22:38:10 +0100

    Create depositToken Page

---
+++ pages/lending-and-borrowing/depositToken.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DepositToken from "../../components/lending/DepositToken";
+
+const depositToken = () => {
+  return (
+    <div>
+      <DepositToken />
+    </div>
+  );
+};
+
+export default depositToken;
