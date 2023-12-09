bea09ccc62a4884fe8d53a773090cca2347a55b2
Great Adams <kingifean@gmail.com>
2023-12-09 05:29:37 +0100

    Added register insurance

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <title>Insurance for Farmers</title>
       </Head>
       <body>
         <Main />
--- pages/parametric-insurance/index.jsx
+++ pages/parametric-insurance/index.jsx
@@ -1,13 +1,51 @@
-import React from "react";
-
+import { useState } from "react";
 import BaseLayout from "layout/Base";
+import ModalComp from "../../components/ModalComp";
 
 const index = () => {
+  const [isRegister, setRegister] = useState(false)
+  const [isFrom, setForm] = useState(false)
   return (
     <BaseLayout>
-      <h2 className="flex text-xl font-bold mt-10 items-center justify-center">
-        Parametric Insurance Page
-      </h2>
+      <section className="mt-10">
+        <button onClick={() => setRegister(true)} className="bg-[#316721] text-white py-4 px-5 font-bold rounded-md">Register Insurance </button>
+        <ModalComp XIcon={true} isOpen={isRegister} onClose={() => setRegister(false)} styling="w-[400px] md:w-[680px] pb-[10px] text-center">
+          {!isFrom ? (
+            <>
+              <img src='/icons/insure.svg' className='m-auto'  />
+              <h1 className="text-[32px] font-bold my-5">Register Insurance</h1>
+              <p className="text-[18px] px-20">Dear customer to register your insurance you need to speak with our agent for proper registration on your insurance plan.</p>
+              <button onClick={() => setForm(true)} className="border border-[#316721] text-[#316721] mt-5 px-5 py-3 rounded-3xl">Send us a message</button>  
+            </>
+          ) :
+
+          (
+            <>
+              <form>
+                <div>
+                  <input type="text" placeholder="Name" className="border p-4 border-[#316721] rounded-lg" />
+                </div>
+                <div className="mt-3">
+                  <input type="email" placeholder="Email" className="border p-4 border-[#316721] rounded-lg" />
+                </div>
+                <div className="mt-3">
+                  <input type="text" placeholder="Location" className="border p-4 border-[#316721] rounded-lg" />
+                </div>
+                <div className="mt-3">
+                  <input type="text" placeholder="Subject" className="border p-4 border-[#316721] rounded-lg" />
+                </div>
+                <div className="mt-3">
+                  {/* <input type="text" placeholder="Subject" /> */}
+                  <textarea name="" id="" cols="21" rows="5" placeholder="Message" className="border p-4 border-[#316721] rounded-lg" ></textarea>
+                </div>
+                <button  className="border border-[#316721] text-[#316721] mt-5 px-5 py-3 rounded-3xl">Send message</button> 
+              </form>
+            </>
+          )
+
+          }
+        </ModalComp>
+      </section>
     </BaseLayout>
   );
 };
