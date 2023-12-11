783b73026a54d27777a9c14a4988b1f631f79ea8
Bright Lazarus <lazbright1@gmail.com>
2023-12-11 01:38:59 +0100

    Update the navbar to be more responsive

--- components/common/MobileHeader.jsx
+++ components/common/MobileHeader.jsx
@@ -1,55 +1,106 @@
-import { useState } from 'react'
-import Link from 'next/link'
-import { MagnifyingGlassIcon, Bars2Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useRouter } from 'next/router';
-import Logo from './Logo';
-import ConnectWallet from './ConnectWallet';
-
+import { useState } from "react";
+import Link from "next/link";
+import {
+  MagnifyingGlassIcon,
+  Bars2Icon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+import Logo from "./Logo";
+import ConnectWallet from "./ConnectWallet";
 
 function MobileHeader() {
-  const [open, setOpen] = useState(false)
-  const router = useRouter()
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
   return (
     <>
-      <header className='lg:hidden padding pt-5'>
-        <nav className='w-full'>
-          <div className='flex justify-between'>
+      <header className="xl:hidden padding pt-5">
+        <nav className="w-full">
+          <div className="flex justify-between">
             {/* Logo and search bar */}
-            <div className='flex space-x-5'>
-            <Link href="/">
-              <Logo />
-            </Link>
-              <div className='pt-1 flex'>
-              </div>
+            <div className="flex space-x-5">
+              <Link href="/">
+                <Logo />
+              </Link>
+              <div className="pt-1 flex"></div>
             </div>
-            {!open ? 
-            <Bars2Icon className='w-8 text-[#446119] cursor-pointer' onClick={() => setOpen(true)} /> 
-            : <XMarkIcon className='w-8 text-[#446119] cursor-pointer'  onClick={() => setOpen(false)}  />}
+            {!open ? (
+              <Bars2Icon
+                className="w-8 text-[#446119] cursor-pointer"
+                onClick={() => setOpen(true)}
+              />
+            ) : (
+              <XMarkIcon
+                className="w-8 text-[#446119] cursor-pointer"
+                onClick={() => setOpen(false)}
+              />
+            )}
           </div>
         </nav>
       </header>
-      {!open ? <div></div>
-        :
-        <div className='fixed h-[100vh] z-10 bg-white w-full px-5 py-2 transition-all'>
-        
-        <div className='space-x-4 pt-4'>
-          <div className="block cursor-pointer font-bold pb-5 ">
-          <h1>About</h1>
-          <h1>Contact</h1>
-          <Link className={router.pathname.includes('lending-and-borrowing') ? 'border-b-2 border-[#87C232]' : ''} href="lending-and-borrowing">Lending</Link>
-          <br />
-          <Link className={router.pathname.includes('parametric-insurance') ? 'border-b-2 border-[#87C232]' : ''} href="parametric-insurance">Insurance</Link>
-          <br />
-          <Link className={router.pathname.includes('asset-bridge') ? 'border-b-2 border-[#87C232]' : ''} href="asset-bridge">Asset Bridge</Link>
-          <br />
-          <Link className={router.pathname.includes('team') ? 'border-b-2 border-[#87C232]' : ''} href="team">Team</Link>
-        </div>
+      {!open ? (
+        <div></div>
+      ) : (
+        <div className="fixed h-[100vh] z-50 bg-white w-full px-5 py-2 transition-all">
+          <div className="gap-x-4 pt-4">
+            <div className="block space-y-6 cursor-pointer font-bold pb-5 ">
+              <h1>About</h1>
+              <h1>Contact</h1>
+              <div>
+                <Link
+                  className={
+                    router.pathname.includes("lending-and-borrowing")
+                      ? "border-b-2 border-[#87C232]"
+                      : ""
+                  }
+                  href="lending-and-borrowing"
+                >
+                  Lending
+                </Link>
+              </div>
+              <div>
+                <Link
+                  className={
+                    router.pathname.includes("parametric-insurance")
+                      ? "border-b-2 border-[#87C232]"
+                      : ""
+                  }
+                  href="parametric-insurance"
+                >
+                  Insurance
+                </Link>
+              </div>
+              <div>
+                <Link
+                  className={
+                    router.pathname.includes("asset-bridge")
+                      ? "border-b-2 border-[#87C232]"
+                      : ""
+                  }
+                  href="asset-bridge"
+                >
+                  Asset Bridge
+                </Link>
+              </div>
+              <div>
+                <Link
+                  className={
+                    router.pathname.includes("team")
+                      ? "border-b-2 border-[#87C232]"
+                      : ""
+                  }
+                  href="team"
+                >
+                  Team
+                </Link>
+              </div>
+            </div>
             <ConnectWallet />
           </div>
-      </div>
-      }
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default MobileHeader
+export default MobileHeader;
--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -1,22 +1,58 @@
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Logo from "./Logo";
 import ConnectWallet from "./ConnectWallet";
 import Link from "next/link";
 
 function Navbar() {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <nav className="hidden padding text-[#141414] mt-5 lg:flex justify-between items-center ">
-      <Link href="/">
+    <nav className="hidden padding text-[#141414] mt-5 xl:flex justify-between items-center ">
+      <Link className="cursor-pointer" href="/">
         <Logo />
       </Link>
-      <div className="flex space-x-10 cursor-pointer font-bold ">
+      <div className="flex space-x-10  font-bold ">
         <h1>About</h1>
         <h1>Contact</h1>
-        <Link className={router.pathname.includes('lending-and-borrowing') ? 'border-b-2 border-[#87C232]' : ''} href="lending-and-borrowing">Lending</Link>
-        <Link className={router.pathname.includes('parametric-insurance') ? 'border-b-2 border-[#87C232]' : ''} href="parametric-insurance">Insurance</Link>
-        <Link className={router.pathname.includes('asset-bridge') ? 'border-b-2 border-[#87C232]' : ''} href="asset-bridge">Asset Bridge</Link>
-        <Link className={router.pathname.includes('team') ? 'border-b-2 border-[#87C232]' : ''} href="team">Team</Link>
+        <Link
+          className={
+            router.pathname.includes("lending-and-borrowing")
+              ? "border-b-2 border-[#87C232] cursor-pointer"
+              : ""
+          }
+          href="lending-and-borrowing"
+        >
+          Lending
+        </Link>
+        <Link
+          className={
+            router.pathname.includes("parametric-insurance")
+              ? "border-b-2 border-[#87C232] cursor-pointer"
+              : ""
+          }
+          href="parametric-insurance"
+        >
+          Insurance
+        </Link>
+        <Link
+          className={
+            router.pathname.includes("asset-bridge")
+              ? "border-b-2 border-[#87C232] cursor-pointer"
+              : ""
+          }
+          href="asset-bridge"
+        >
+          Asset Bridge
+        </Link>
+        <Link
+          className={
+            router.pathname.includes("team")
+              ? "border-b-2 border-[#87C232] cursor-pointer"
+              : ""
+          }
+          href="team"
+        >
+          Team
+        </Link>
       </div>
       <ConnectWallet />
     </nav>
